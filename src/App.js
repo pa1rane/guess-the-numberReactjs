f1d5e65170ec26f9407e15487c27926e9ae735df
Pavan Rane <ranepavanrp@gmail.com>
2023-10-06 19:27:41 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+ 
+  const randomNum = (Math.random() * 100).toFixed(0);
+
+  const [input, setInput] = useState("");
+  const [num, setNum] = useState(randomNum);
+  const [guessState, setGuessState] = useState(" ");
+  const [guesses, setGuesses] = useState([input]);
+
+  
+  const handleInput = (e) => {
+    setInput(e.target.value);
+  }
+
+  const handleSubmit = () => {
+    if (input.trim() !== "") {
+    setGuesses((prev) => ([...prev, input]))
+    if(input > num) {
+      setGuessState("To High")
+    } else if (input < num) {
+      setGuessState("To Low")
+    } else {
+      setGuessState("You are absolutely Correct")
+    }
+  }
+    setInput(" ");
+  
+  }
+ 
+  const handleReset = () => {
+    setNum((Math.random() * 100).toFixed(0));
+    setGuessState(" ");
+    setInput(" ");
+    setGuesses([]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>Enter a Guess Between 0 to 100</h1>
+      <br />
+      <input type="number" onChange={handleInput} disabled={guesses.length >= 10 ? true : false} value={input} placeholder='Enter a number'/>
+      <br />
+      <div>
+        <button onClick={handleSubmit} disabled={guesses.length >= 10 ? true : false}>Submit</button>
+        <button onClick={handleReset} disabled={guesses.length >= 10 ? false : true}>Start Game</button>
+        <p>{guessState}</p>
+        <div>
+          <p>Your Guesses : </p>
+          {guesses.map((guess, index) => (
+            <p key={index}>{guess},</p>
+          ))}
+          </div>
+      </div>
+    </>
+  )
 }
 
 export default App;
